638fad9c4c7c2e5f6b4a0899903da059f0904d42
Murad Ali Ahmad Hassan <mura0273@edu.eal.dk>
2019-02-25 10:55:51 +0100

    Add project files.

---
+++ UnitTestProject1/ClubMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public enum Gender { Male, Female };
+    public class ClubMember : IComparable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}: {FirstName} {LastName} ({Gender}, {Age} years)";
+        }
+
+        public int CompareTo(object obj)
+        {
+            ClubMember clubmember = (ClubMember)(obj);
+            return this.FirstName.CompareTo(clubmember.FirstName);
+        }
+    }
+
+}
+
+// hej morten, xd. Hilsen Pjæk-tor
+//kom i morgen xdxd
+// okay x)
+//*/ victor du fucker koden over xxxxxxxxxxxdDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// jeg skal nok gå nu. GL HF ses i morgen
